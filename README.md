7906f537fd8f5d5c9a5580fb908519e1568c735e
Yi Jin <yi.jin@Yis-MacBook-Pro.local>
2020-07-27 20:47:53 -0400

    add content

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Linkedin-Job-Scraper
-Scraping Linked Jobs
+
+The tool is to scraping Linkedin jobs by using selenium. You will need to login with your account specified in `config.json`. Then you will be able to scraping the jobs on linkedin.
+
+## Preparation
+
+You have to download chromedriver from [here](https://sites.google.com/a/chromium.org/chromedriver/home). Select relevant version for your chrome.
+
+Install required library `pip install -r requirements.txt`.
+
+Fill in the parameter in `config.json` with the following
+
+## run
+
+Now you can run the code py `python main.py`. This will launch the chrome to do the work.
---
+++ main.py
@@ -0,0 +1,29 @@
+from scrapejobs import LinkedinScrap
+import json
+
+
+if __name__ == '__main__':
+    # get config
+    with open("config.json", "r") as config_file:
+        config = json.load(config_file)
+    
+    username = config.get('username')
+    password = config.get('password')
+    language = config.get('language')
+    position = config.get('position')
+    location = config.get('location')
+
+    # print input
+    print("\ninput:")
+
+    print  ("\nUsername:  "+ username,
+        "\nPassword:  "+ password,
+        "\nLanguage:  "+ language,
+        "\nPosition:  "+ position,
+        "\nLocation:  "+ location)
+    
+    print("\nLet's scrape some jobs!\n")
+    
+    # start
+    bot = LinkedinScrap(username,password, language, position, location)
+    bot.start_scrape()
